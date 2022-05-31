3932636d72c7c1cb4a555bb05dfce24bd73eaaec
EinKomischerTyp2 <simonschepers@web.de>
2022-05-31 20:25:23 +0200

    Basic Movement Script (ohne Groundcheck)
    Script zum testen. (Wird noch verbessert und ausgebaut.

--- Studyweek1_2022/Assets/Scripts/PlayerBasicMovement.cs
+++ Studyweek1_2022/Assets/Scripts/PlayerBasicMovement.cs
@@ -6,20 +6,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerBasicMovement : MonoBehaviour
 {
+    
     private PlayerMovement_Controls playerControls;
     private InputAction groundMovement;
-  
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D playerRB;
+    [SerializeField] private float jumpForce;
+    private float normalGravityScale = 1.5f;
+    private float fallingGravityScale = 1.1f;
+    private float moveInput;
+    public float runMaxSpeed = 3;
+    private float velocityX;
+
     private void Awake()
     {
         playerControls = new PlayerMovement_Controls();
+        playerRB = GetComponent<Rigidbody2D>();
+        groundMovement = playerControls.Player.GroundMovement;
     }
     private void OnEnable()
-    {      
-        groundMovement = playerControls.Player.GroundMovement;
+    {            
         groundMovement.Enable();
         playerControls.Player.Jump.performed += playerJump;
         playerControls.Player.Jump.Enable();       
@@ -30,15 +35,32 @@ public class PlayerBasicMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Debug.Log("MovementValues" + groundMovement.ReadValue<float>());
-    }
-    void Update()
-    {
-        
-    }
+        Debug.Log(moveInput);
+        velocityX = transform.position.x + runMaxSpeed * moveInput * Time.deltaTime;
+        moveInput = groundMovement.ReadValue<float>();
+        if(moveInput > 0)
+        {
+            transform.position = new Vector2(velocityX, transform.position.y);
+        }
+        else if (moveInput < 0)
+        {
+            
+            transform.position = new Vector2(velocityX, transform.position.y);
+        }
+        if (playerRB.velocity.y >= 0)
+        {
+            playerRB.gravityScale = normalGravityScale;
+        }
+        else if (playerRB.velocity.y < 0)
+        {
+            playerRB.gravityScale *= fallingGravityScale;           
+        }
+    } 
     private void playerJump(InputAction.CallbackContext obj)
     {
-        Debug.Log("I jumped!");
+        Debug.Log("I jumped!");       
+        playerRB.AddForce( new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        
     }
 
 
