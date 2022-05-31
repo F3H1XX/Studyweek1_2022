1e898bbe70458e2da6c3d0154c0dd4fcff665571
EinKomischerTyp2 <simonschepers@web.de>
2022-05-31 21:24:32 +0200

    Movement ist kaputt. Das wird jetzt ein Sackhüpfspiel

--- Studyweek1_2022/Assets/Scripts/PlayerBasicMovement.cs
+++ Studyweek1_2022/Assets/Scripts/PlayerBasicMovement.cs
@@ -14,8 +14,10 @@ public class PlayerBasicMovement : MonoBehaviour
     private float normalGravityScale = 1.5f;
     private float fallingGravityScale = 1.1f;
     private float moveInput;
-    public float runMaxSpeed = 3;
+    public float runMaxSpeed = 4;
     private float velocityX;
+    private float acceleration = 2;
+    private float decceleration = 3;
 
     private void Awake()
     {
@@ -35,18 +37,17 @@ public class PlayerBasicMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Debug.Log(moveInput);
-        velocityX = transform.position.x + runMaxSpeed * moveInput * Time.deltaTime;
         moveInput = groundMovement.ReadValue<float>();
-        if(moveInput > 0)
-        {
-            transform.position = new Vector2(velocityX, transform.position.y);
-        }
-        else if (moveInput < 0)
-        {
-            
-            transform.position = new Vector2(velocityX, transform.position.y);
-        }
+        float targetSpeed = moveInput * runMaxSpeed;
+        float speedDiff =  targetSpeed - playerRB.velocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, 0.87f) * Mathf.Sign(speedDiff);
+
+        playerRB.AddForce(Vector2.right * movement);
+        Debug.Log(movement);
+        
+        
+        
         if (playerRB.velocity.y >= 0)
         {
             playerRB.gravityScale = normalGravityScale;
@@ -60,7 +61,7 @@ public class PlayerBasicMovement : MonoBehaviour
     {
         Debug.Log("I jumped!");       
         playerRB.AddForce( new Vector2(0, jumpForce), ForceMode2D.Impulse);
-        
+        Debug.Log(moveInput);
     }
 
 
