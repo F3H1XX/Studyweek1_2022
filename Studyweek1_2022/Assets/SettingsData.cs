cd0b227043463e56f8ca7d8d51b87c4918322f91
Kevin Vogts <Kevin@vogts.family>
2022-06-01 14:50:57 +0200

    Menu Done

---
+++ Studyweek1_2022/Assets/SettingsData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SettingsData", menuName = "SizeRumble/SettingsData")]
+public class SettingsData : ScriptableObject
+{
+    public bool EnableWallJump;
+    public bool EnableDoubleJump;
+}
