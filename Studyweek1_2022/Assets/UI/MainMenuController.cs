b7fac5ff4dced7145392b93c75460f9c6154d45e
Kevin Vogts <Kevin@Vogts.family>
2022-06-03 01:52:16 +0200

    Build4_Final

--- Studyweek1_2022/Assets/UI/MainMenuController.cs
+++ Studyweek1_2022/Assets/UI/MainMenuController.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
