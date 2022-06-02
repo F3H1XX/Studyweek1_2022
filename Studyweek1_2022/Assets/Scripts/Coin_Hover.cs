342e929c16c98ca2c087e533d1e916a6d6774308
EinKomischerTyp2 <simonschepers@web.de>
2022-06-02 19:12:45 +0200

    Todescoin hinzugefügt
    They spin me right round!

--- Studyweek1_2022/Assets/Scripts/Coin_Hover.cs
+++ Studyweek1_2022/Assets/Scripts/Coin_Hover.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Coin_Hover : MonoBehaviour
 {
+    private Rigidbody2D coinRB;
     private Vector2 startPosition;
     private void Awake()
     {
         startPosition = transform.position;
+        coinRB = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = startPosition + Vector2.down * Mathf.Sin(Time.time);
+        coinRB.AddTorque(1, ForceMode2D.Force);
     }
 }
